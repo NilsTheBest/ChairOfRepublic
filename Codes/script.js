44045d78eaf490b6e615d8c77de20b55dc8f9ff0
ajsya <52182942+ajsya@users.noreply.github.com>
2020-08-10 19:22:20 -0400

    Create script.js

---
+++ Codes/script.js
@@ -0,0 +1,17 @@
+function sendMessage() {
+  var request = new XMLHttpRequest();
+  request.open(
+    "POST",
+    "https://discordapp.com/api/webhooks/742522935427989676/r0vZWi5dy0QLh0V_NUgUcxwznCl7Rbw47ud_ynqPjEO5dtPXtoMSnIs62uvThacaU6R0"
+  );
+  request.setRequestHeader("Content-type", "application/json");
+  var message_content = document.getElementById("Message").value;
+  var username = document.getElementById("username").value;
+  var params = {
+    username: username,
+    avatar_url: "",
+    content: message_content
+  };
+  request.send(JSON.stringify(params));
+  alert("Message Sent!")
+}
